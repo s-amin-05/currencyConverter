ebc0f8a5dfb7898ce3641de56369002b0b6182b7
Amin Solkar <aminsolkar5@gmail.com>
2024-06-24 22:48:38 +0530

    Added functionality to calculate exchange values and update the display everytime the input changes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 //Part1 : setting up all the option tags
-let currency = ''
+let currencyLink = ''
 
-let URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies${currency}.json`
+let URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies${currencyLink}.json`
 
 
-// Accessed the data from the API
+//Used querySelectorAll to select both the select tags
+const currencyList = document.querySelectorAll('.currency')
+
+const selectFrom = currencyList[0]
+const selectTo = currencyList[1]
+const inputBox = document.querySelector('#input_box');
+const outputBox = document.querySelector('#output_box');
+
 
 let currencyObj = {};
 
+//A function to add all currencies as options using API
 
 (async ()=> {
   let response = await fetch(URL)
@@ -20,8 +28,8 @@ let currencyObj = {};
     currencyOption.innerText = key.toUpperCase()
     //Used cloneNode here because the appendChild method cut-pastes currencyOption node to only one of the select tags
     const clone = currencyOption.cloneNode(true)
-    currencyList[0].appendChild(currencyOption);
-    currencyList[1].appendChild(clone);
+    selectFrom.appendChild(currencyOption);
+    selectTo.appendChild(clone);
     //To add a default selected option in both select tags
     if(currencyOption.value === 'inr')
        currencyOption.selected = true
@@ -31,19 +39,51 @@ let currencyObj = {};
   }
 })();
 
-// Accessed the both the select tags 
-let currencyList = document.querySelectorAll('.currency')
+//Part2: Accessing currency exchange values and using them
 
 
 
+const convertCurrency = async () =>{
+  
+  let baseCurrency = selectFrom.value;
+  let exchangeCurrency = selectTo.value;
 
+  let baseCurrencyValue = inputBox.value;
 
+  if (baseCurrencyValue === "" || baseCurrencyValue < 1) {
+    baseCurrencyValue = 1;
+    baseCurrency.value = "1";
+  }
+  
+  currencyLink = '/' + baseCurrency;
+  URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies${currencyLink}.json`;
+  
+  
+  let res = await fetch(URL);
+  let data = await res.json();
+  let conversionRate = Number(data[baseCurrency][exchangeCurrency]);
+  let exchangeCurrencyValue = conversionRate * baseCurrencyValue;
+  outputBox.value = `${exchangeCurrencyValue}`;
+  
+}
 
+selectFrom.addEventListener('change', (e)=>{
+  e.preventDefault();
+  convertCurrency();
+}
+)
 
+selectTo.addEventListener('change', (e)=>{
+  e.preventDefault();
+  convertCurrency();
+}
+)
 
-
-
-
+inputBox.addEventListener('input', (e)=>{
+  e.preventDefault();
+  convertCurrency()
+}
+)
 
 
 
